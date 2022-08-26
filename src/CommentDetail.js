ec090902c835d1d8c7ba5f55cd0b0a1649229895
Dicki Prasetya <semenjakpetang176@gmail.com>
2022-08-27 00:08:50 +0700

    psuh 2

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,23 +1,24 @@
 import React from "react";
 import faker from "faker";
 
-const CommentDetail = () => {
+function CommentDetail(props) {
+  //   console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.image()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          {faker.name.firstName("female" | "male")}
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">{faker.date.month()}</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice Blog</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
-};
+}
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { faker } from "@faker-js/faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 // let le = document.getElementById('root');
 
 const App = () => {
+  var date = "Time Ago " + new Date().toLocaleTimeString();
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard>
+        <CommentDetail
+          author="sam"
+          timeAgo={date}
+          content="nice blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <CommentDetail
+        author="diki"
+        timeAgo={date}
+        content="wow your amazing"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="iqbal"
+        timeAgo={date}
+        content="nice blog bro!"
+        avatar={faker.image.image()}
+      />
     </div>
   );
 };
